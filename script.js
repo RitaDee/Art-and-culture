01dc2e5e95ca02e0f8dafd4dc5903d66e7c5bba0
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-01-11 00:14:03 +0100

    Create dynamic section

--- script.js
+++ script.js
@@ -5,3 +5,73 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   topMenu.classList.toggle('active');
 });
+
+document.querySelectorAll('.nav-link').forEach((n) => n
+  .addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    topMenu.classList.remove('active');
+  }));
+
+// pop-up menu
+const speakers = [{
+  id: '1',
+  speakerName: 'Yochai Benkler',
+  speakerImg: './images/speaker_01.png',
+  speakerPost: 'Havard Law School',
+  speakerInfo: 'Lorem ipsum dolor sit amet. Ex ut amet adipisci qui voluptatem atque hic voluptates magnam est incidunvoluptate eos sint consequatur.',
+},
+
+{
+  id: '2',
+  speakerName: 'SohYeong Noh',
+  speakerImg: './images/speaker_02.png',
+  speakerPost: 'Art and Humanities Lagos, Nigeria',
+  speakerInfo: 'Lorem ipsum dolor sit amet. Ex ut amet adipisci qui voluptatem atque hic voluptates magnam est incidunvoluptate eos sint consequatur.',
+},
+
+{
+  id: '3',
+  speakerName: 'Gilnam Jeon',
+  speakerImg: './images/speaker_03.png',
+  speakerPost: 'Cultural Heritage Los Angeles',
+  speakerInfo: 'Lorem ipsum dolor sit amet. Ex ut amet adipisci qui voluptatem atque hic voluptates magnam est incidunvoluptate eos sint consequatur.',
+},
+
+{
+  id: '4',
+  speakerName: 'Julia Leda',
+  speakerImg: './images/speaker_04.png',
+  speakerPost: 'Art and Culture India',
+  speakerInfo: 'Lorem ipsum dolor sit amet. Ex ut amet adipisci qui voluptatem atque hic voluptates magnam est incidunvoluptate eos sint consequatur.',
+},
+
+{
+  id: '5',
+  speakerName: 'Lyla Tretikov',
+  speakerImg: './images/speaker_05.png',
+  speakerPost: 'Havard Law School',
+  speakerInfo: 'Lorem ipsum dolor sit amet. Ex ut amet adipisci qui voluptatem atque hic voluptates magnam est incidunvoluptate eos sint consequatur.',
+},
+
+{
+  id: '6',
+  speakerName: 'Ryan Merkly',
+  speakerImg: './images/speaker_06.png',
+  speakerPost: 'Havard Law School',
+  speakerInfo: 'Lorem ipsum dolor sit amet. Ex ut amet adipisci qui voluptatem atque hic voluptates magnam est incidunvoluptate eos sint consequatur.',
+},
+];
+
+const guestSpeakers = document.querySelector('.speakers');
+
+for (let i = 0; i < speakers.length; i += 1) {
+  guestSpeakers.innerHTML += `
+  <div class="speakers">
+    <img src="${speakers[i].speakerImg}" alt="image">
+            <div class="speaker-info">
+            <h3>${speakers[i].speakerName}</h3>
+            <h4>${speakers[i].speakerPost}</h4>
+            <span class="speaker-bar"></span>
+            <p>${speakers[i].speakerInfo}</p>
+            </div>`;
+}
