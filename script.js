e3ef4979956be10d6a983c56f6b2d1ee2bb5254b
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-01-11 13:23:20 +0100

    Implement functional on hamburger

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const speakers = [{
   speakerName: 'SohYeong Noh',
   speakerImg: './images/speaker_02.png',
   speakerPost: 'Art and Humanities Lagos, Nigeria',
-  speakerInfo: 'Lorem ipsum dolor sit amet. Ex ut amet adipisci qui voluptatem atque hic voluptates magnam est incidunvoluptate eos sint consequatur.',
+  speakerInfo: 'Lorem ipsum dolor sit amet. Ex ut amet adipisci qui voluptatem atque hic eos sint consequatur.',
 },
 
 {
@@ -34,7 +34,7 @@ const speakers = [{
   speakerName: 'Gilnam Jeon',
   speakerImg: './images/speaker_03.png',
   speakerPost: 'Cultural Heritage Los Angeles',
-  speakerInfo: 'Lorem ipsum dolor sit amet. Ex ut amet adipisci qui voluptatem atque hic voluptates magnam est incidunvoluptate eos sint consequatur.',
+  speakerInfo: 'Lorem ipsum dolor sit amet. Ex ut amet adipisci qui idunvoluptate eos sint consequatur.',
 },
 
 {
@@ -42,7 +42,7 @@ const speakers = [{
   speakerName: 'Julia Leda',
   speakerImg: './images/speaker_04.png',
   speakerPost: 'Art and Culture India',
-  speakerInfo: 'Lorem ipsum dolor sit amet. Ex ut amet adipisci qui voluptatem atque hic voluptates magnam est incidunvoluptate eos sint consequatur.',
+  speakerInfo: 'Lorem ipsum dolor sit amet. Ex ut amet adipisci qui eos sint consequatur.',
 },
 
 {
@@ -50,7 +50,7 @@ const speakers = [{
   speakerName: 'Lyla Tretikov',
   speakerImg: './images/speaker_05.png',
   speakerPost: 'Havard Law School',
-  speakerInfo: 'Lorem ipsum dolor sit amet. Ex ut amet adipisci qui voluptatem atque hic voluptates magnam est incidunvoluptate eos sint consequatur.',
+  speakerInfo: 'Lorem ipsum dolor sit amet. Ex ut amet adipisci qui incidunvoluptate eos sint consequatur.',
 },
 
 {
@@ -58,20 +58,21 @@ const speakers = [{
   speakerName: 'Ryan Merkly',
   speakerImg: './images/speaker_06.png',
   speakerPost: 'Havard Law School',
-  speakerInfo: 'Lorem ipsum dolor sit amet. Ex ut amet adipisci qui voluptatem atque hic voluptates magnam est incidunvoluptate eos sint consequatur.',
+  speakerInfo: 'Lorem ipsum dolor sit amet. Ex ut amet adipisci hic voluptates magnam est incidunvoluptate eos sint consequatur.',
 },
 ];
 
-const guestSpeakers = document.querySelector('.speakers');
+const guestSpeakers = document.querySelector('.speakers-container');
 
 for (let i = 0; i < speakers.length; i += 1) {
   guestSpeakers.innerHTML += `
   <div class="speakers">
-    <img src="${speakers[i].speakerImg}" alt="image">
+    <div><img src="${speakers[i].speakerImg}" alt="image"></div>
             <div class="speaker-info">
             <h3>${speakers[i].speakerName}</h3>
             <h4>${speakers[i].speakerPost}</h4>
             <span class="speaker-bar"></span>
-            <p>${speakers[i].speakerInfo}</p>
-            </div>`;
+            <p>${speakers[i].speakerInfo}</p>         
+             </div>
+             </div>`;
 }
